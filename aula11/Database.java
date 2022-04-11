37f14c14e3f892d333706fade661f62542534520
Jéssica Gelsdorf <82670472+mejessica@users.noreply.github.com>
2022-04-11 11:15:57 -0300

    Create Database.java

---
+++ aula11/Database.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class Database{
+
+    public static void main (String[] args){
+        DVD a = new DVD("quebrando a banca","naosei","1", "30000");
+        CD a = new CD("ly","bts","8","30000");
+    }
+}
