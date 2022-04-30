33a4a5beb8c5346eb6756fee6cb14ac4f078ebfe
Jéssica Gelsdorf <82670472+mejessica@users.noreply.github.com>
2022-04-30 15:15:54 -0300

    Update Aluno.java

--- aluno/Aluno.java
+++ aluno/Aluno.java
@@ -1,32 +1,33 @@
-public class TesteAluno {
-	public static void main(String[] args) {
-		Aluno a1 = new Aluno();
-		a1.nome = "roberta";
-		a1.idade = 16;
-		a1.nota1 = 0;
-		a1.nota2 = 0;
-		a1.nota3 = 0;
-		a1.notasInseridas = 0;
-		
-		
-		a1.fazAniversario();
-		System.out.println("Idade atual agora "+a1.idade);
-		System.out.println("Nome: " +a1.nome+ " /Idade: " + a1.idade++);
-		a1.fazAniversario();
-		a1.fazAniversario();
-		System.out.println("Idade atual agora "+a1.idade);
-		
-		a1.insereNota(8);
-		System.out.println("media "+a1.calculaMedia());
-		a1.insereNota(10);
-		System.out.println("media "+a1.calculaMedia());
-		a1.insereNota(4);
-		System.out.println("media: "+a1.calculaMedia());
-		
+public class Aluno {
+	
+	
 
-		
-		
-     
+	String nome;
+	int idade;
+	int nota1;
+	int nota2;
+	int nota3;
+	int notasInseridas;
+	
+	void fazAniversario() {
+		this.idade++;
+	}
+	void insereNota (int nota) {
+		if(this.notasInseridas == 0) {
+			this.nota1 = nota;
+			this.notasInseridas++;
+		} else if(this.notasInseridas == 1) {
+			this.nota2 = nota;
+			this.notasInseridas++;
+		} else if(this.notasInseridas == 2) {
+			this.nota3 = nota;
+			this.notasInseridas++;
+		}
+ }
+	
+	double calculaMedia() {
+		return (this.nota1 + this.nota2 + this.nota3) / (double) this.notasInseridas;
+	
 	}
 
 }
