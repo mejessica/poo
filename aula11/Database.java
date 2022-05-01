1d817de5a8f05e48ea2973d2d62301fe309fc720
Jéssica Gelsdorf <82670472+mejessica@users.noreply.github.com>
2022-05-01 19:56:13 -0300

    Update Database.java

--- aula11/Database.java
+++ aula11/Database.java
@@ -2,8 +2,22 @@ import java.util.ArrayList;
 
 public class Database{
 
+    static ArrayList<CD> cds = new ArrayList<>();
+    static ArrayList<DVD> dvds = new ArrayList<>();
+
+    static void ImprimeLista(ArrayList hehe) {
+        for (int i = 0; i <hehe.size(); i++){
+            System.out.println(hehe.get(i));
+        }
+    }
     public static void main (String[] args){
-        DVD a = new DVD("quebrando a banca","naosei","1", "30000");
-        CD a = new CD("ly","bts","8","30000");
+        CD cdzinho = new CD ("ela partiu", "tim maia", 897, "4:32");
+
+        cds.add(cdzinho);
+        ImprimeLista(cds);
+
+        DVD dvdzinho = new DVD ("10 cavalos", "Pedro sampaio", 24, "5:24");
+        dvds.add(dvdzinho);
+        ImprimeLista(dvds);
     }
 }
